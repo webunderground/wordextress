fee7e7862d8e49d5d07f4f0b08064414e330e85e
webunderground <101758687+webunderground@users.noreply.github.com>
2022-05-17 19:25:17 -0500

    Add files via upload

---
+++ db/db_connection.php
@@ -0,0 +1,10 @@
+<?php
+require_once('mysql.php');
+
+$serverhost = "mysql.webcindario.com";
+$serveruser = "online1973";
+$serverpwd  = "lolita1873";
+$dbname     = "online1973";
+
+$MyDb       = new cMysqlDB($serverhost,$serveruser,$serverpwd,$dbname);
+?>
